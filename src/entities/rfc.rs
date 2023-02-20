c203abcdcfb47f955c3e7f13dbed3d3a14c66589
Austin Reifsteck <austin.reifsteck@covermymeds.com>
2023-02-20 09:48:30 -0500

    it's almost working, I swear

--- src/entities/rfc.rs
+++ src/entities/rfc.rs
@@ -1,12 +1,15 @@
-use sea_query::types::IntoTableRef;
-use sea_query::Iden;
-use sea_query::Query;
+use sea_query::expr::SimpleExpr;
+use sea_query::query::Query;
+use sea_query::types::Alias;
+use sea_query::{Iden, PostgresQueryBuilder};
+use sqlx::postgres::PgRow;
 use sqlx::types::chrono::{DateTime, Utc};
-use sqlx::{Error, PgPool};
+use sqlx::{Error, Executor, FromRow, PgPool, Postgres, QueryBuilder};
 use std::collections::HashSet;
 use std::hash::Hash;
+use strum_macros::AsRefStr;
 
-#[derive(Debug)]
+#[derive(Debug, sqlx::FromRow)]
 pub struct RFC {
     pub id: i32,
     pub status: Status,
@@ -17,19 +20,46 @@ pub struct RFC {
     pub updated_at: DateTime<Utc>,
 }
 
-#[derive(Iden)]
-enum Columns {
+#[derive(AsRefStr, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Status {
+    #[strum(serialize = "active")]
+    Active,
+    #[strum(serialize = "approved")]
+    Approved,
+    #[strum(serialize = "denied")]
+    Denied,
+    #[strum(serialize = "discarded")]
+    Discarded,
+}
+pub trait Referenceable {
+    fn table_ref() -> Alias;
+}
+pub trait Valuable {
+    fn value(&self) -> SimpleExpr;
+}
+
+impl Referenceable for RFC {
+    fn table_ref() -> Alias {
+        Alias::new("request_for_comments")
+    }
+}
+
+#[derive(Iden, Clone, PartialEq, Eq, Hash)]
+enum RequestForComments {
     Status(Status),
     Proposal(String),
 }
 
-#[test]
-fn test() {
-    assert_eq!(Columns::Status(Status::Active).to_string(), "status");
+impl Valuable for RequestForComments {
+    fn value(&self) -> SimpleExpr {
+        match self {
+            Self::Status(status) => status.as_ref().into(),
+            Self::Proposal(p) => p.into(),
+        }
+    }
 }
 
 pub struct Changeset<T, D> {
-    for_table: &'static str,
     changes: HashSet<T>,
     data: Option<D>,
     valid: bool,
@@ -38,61 +68,50 @@ pub struct Changeset<T, D> {
 // How to turn changeset -> qurey
 // How to do custom/generic validations
 // How do only enforce one change of a particular type
-pub trait storable {
-    fn table_name() {}
-}
 
 impl<T, D> Changeset<T, D>
 where
-    T: Eq + Hash + Iden + IntoTableRef,
+    T: Eq + Hash + Iden + Valuable + Clone + 'static,
+    for<'a> D: Referenceable + sqlx::FromRow<'a, PgRow>,
 {
-    fn new(table: &str, backing_data: Option<D>) -> Self {
+    pub fn new(backing_data: Option<D>) -> Self {
         Changeset {
-            for_table: table,
             changes: HashSet::new(),
             data: backing_data,
             valid: false,
         }
     }
-    fn add_change(&mut self, change: T) -> &mut Self {
+    pub fn add_change(&mut self, change: T) -> &mut Self {
         self.changes.insert(change);
         self
     }
-    fn validate(&mut self) -> Result<&mut Self, Error> {
+    pub fn validate(&mut self) -> Result<&mut Self, Error> {
         self.valid = true;
         Ok(self)
     }
-    //Changeset::new()
-    //.add_change(Proposal("Here is the proposal"))
-    //.add_change(Topic("Here is the Topic"))
-    //.validate()
-    //.insert()
-    //
-
-    //Database operations
-    //Changeset -> query -> database
-    // How to dynamically get the table name?
-    fn insert(&self) -> Result<D, Error> {
-        let query = Query::insert().into_table("What can I put here");
+    pub async fn insert(self, pool: &PgPool) -> Result<D, Error> {
+        pool.fetch_one(self.build_query().as_str())
+            .await
+            .map(|row| D::from_row(&row).unwrap())
+    }
+    fn build_query(&self) -> String {
+        Query::insert()
+            .into_table(D::table_ref())
+            .columns(self.changes.clone())
+            .values_panic(self.changes.clone().iter().map(|x| x.value()))
+            .to_string(PostgresQueryBuilder)
     }
 }
 
-#[derive(Debug)]
-pub enum Status {
-    Active,
-    Approved,
-    Denied,
-    Discarded,
+#[test]
+fn build_query() {
+    let mut cs: Changeset<RequestForComments, RFC> = Changeset::new(None);
+    // Either make order matter or test this indirectly
+    cs.add_change(RequestForComments::Status(Status::Active))
+        .add_change(RequestForComments::Proposal(String::from("Hello")));
 }
 
 pub enum Mode {
     Insert,
     Update,
 }
-
-// #[sqlx::test]
-// async fn works(pool: PgPool) {
-//     let participant = create_one(&pool, "Austin").await.unwrap();
-//     println!("{:?}", participant);
-//     assert_eq!("Austin", participant.username);
-// }
